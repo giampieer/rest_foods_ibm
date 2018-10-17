0f91d165d4a22b800015f8c242d3760f254ddd4e
giampieer <giampieer24@gmail.com>
2018-10-17 17:29:13 -0500

    orden en casos

--- index.php
+++ index.php
@@ -1,16 +1,26 @@
-<?php 
-$ch = curl_init();
-$food = "http://as01.epimg.net/deporteyvida/imagenes/2018/04/04/portada/1522855616_827022_1522855733_noticia_normal.jpg";
-curl_setopt($ch, CURLOPT_URL, "https://gateway.watsonplatform.net/visual-recognition/api/v3/classify?url=".$food."&version=2018-03-19");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POST, 1);
+<?php
+$op = $_REQUEST['op'];
+switch ($op){
+  case 1: {
+    $ch = curl_init();
+    $food = $_REQUEST['food'];
+    curl_setopt($ch, CURLOPT_URL, "https://gateway.watsonplatform.net/visual-recognition/api/v3/classify?url=".$food."&version=2018-03-19");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
 
-curl_setopt($ch, CURLOPT_USERPWD, "apikey" . ":" . "SmfRmQFeWzcXFy-UBATYSsnp8aMjtXDtzZWrGScUbdWY");
+    curl_setopt($ch, CURLOPT_USERPWD, "apikey" . ":" . "SmfRmQFeWzcXFy-UBATYSsnp8aMjtXDtzZWrGScUbdWY");
 
-$result = curl_exec($ch);
-if (curl_errno($ch)) {
-  echo 'Error:' . curl_error($ch);
-} else {
-  echo $result;
+    $result = curl_exec($ch);
+    if (curl_errno($ch)) {
+      echo 'Error:' . curl_error($ch);
+    } else {
+      echo $result;
+    }
+    curl_close ($ch);
+    break;
+  }
+  case 2: {
+    break;
+  }
 }
-curl_close ($ch);
+
