73f687849daa037f0360812b970c0ed92b7a9d16
giampieer <giampieer24@gmail.com>
2018-10-17 17:40:04 -0500

    agegado bot watson como op=2

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ switch ($op){
     curl_setopt($ch, CURLOPT_URL, "https://gateway.watsonplatform.net/visual-recognition/api/v3/classify?url=".$food."&version=2018-03-19");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_POST, 1);
-
     curl_setopt($ch, CURLOPT_USERPWD, "apikey" . ":" . "SmfRmQFeWzcXFy-UBATYSsnp8aMjtXDtzZWrGScUbdWY");
 
     $result = curl_exec($ch);
@@ -20,6 +19,24 @@ switch ($op){
     break;
   }
   case 2: {
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, "https://gateway.watsonplatform.net/assistant/api/v1/workspaces/8469cea4-fde9-415d-9424-b90cb8c5d070/message?version=2018-09-20");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "{\"input\": {\"text\": \"buenas noches\"}}");
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_USERPWD, "3e205e42-1483-40ed-99bb-cf9ac07b5f0d" . ":" . "uXPmzQzfqiSe");
+
+    $headers = array();
+    $headers[] = "Content-Type: application/json";
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    $result = curl_exec($ch);
+    if (curl_errno($ch)) {
+      echo 'Error:' . curl_error($ch);
+    }
+    echo $result;
+    curl_close ($ch);
     break;
   }
 }
