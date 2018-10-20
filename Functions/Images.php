c68b3b5efb36f48d5db8fdfefe689e88f6b03195
giampieer <giampieer24@gmail.com>
2018-10-20 15:02:42 -0500

    add name

--- Functions/Images.php
+++ Functions/Images.php
@@ -2,10 +2,12 @@
 class Images {
   public function generateImage($Base64Img) {
     $data = base64_decode($Base64Img);
+    $name = "";
 // Proporciona una locación a la nueva imagen (con el nombre y formato especifico)
-    $filepath = "/var/www/html/rest_foods_ibm/images/imagen12.jpg"; // or image.jpg
+    $filepath = "/var/www/html/rest_foods_ibm/images/".$name; // or image.jpg
 // Finalmente guarda la imágen en el directorio especificado y con la informacion dada
     file_put_contents($filepath, $data);
+    $url = "http://local.restfoodsibm.com/images/".$name;
     return $data;
   }
 }
